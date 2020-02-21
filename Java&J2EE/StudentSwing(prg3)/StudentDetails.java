d0f6c61acf56e601d0a57f40b6ccac0eb11d36ec
AYUSH UJJWAL <42496674+ayush1612@users.noreply.github.com>
2020-02-21 10:56:54 +0530

    Added rest of the functionalities

--- Java&J2EE/StudentSwing(prg3)/StudentDetails.java
+++ Java&J2EE/StudentSwing(prg3)/StudentDetails.java
@@ -47,7 +47,10 @@ public class StudentDetails implements ActionListener {
     double sgpa[];
     String category;
 
-    JButton jb = new JButton("Confirm");
+    JButton confirm = new JButton("Confirm");
+    JButton compute = new JButton("Compute");
+    JButton done = new JButton("Done");
+
     StudentDetails sd;
     JTextArea ja = new JTextArea(15, 30);
 
@@ -86,8 +89,14 @@ public class StudentDetails implements ActionListener {
         frm.add(jl6);
         frm.add(jt6);
         frm.add(jp3);
-        jp3.add(jb);
-        jb.addActionListener(this);
+        jp3.add(confirm);
+        jp3.add(compute);
+        jp3.add(done);
+
+        confirm.addActionListener(this);
+        compute.addActionListener(this);
+        done.addActionListener(this);
+
         frm.add(ja);
 
         frm.setVisible(true);
@@ -98,32 +107,83 @@ public class StudentDetails implements ActionListener {
 
     public void actionPerformed(ActionEvent evt) {
 
-        name = jt1.getText();
-        usn = jt2.getText();
-        age = Integer.parseInt(jt3.getText());
-        address = jt4.getText();
-        sgpa = new double[8];
-        sgpa[0] = Double.parseDouble(jt51.getText());
-        sgpa[1] = Double.parseDouble(jt52.getText());
-        sgpa[2] = Double.parseDouble(jt53.getText());
-        sgpa[3] = Double.parseDouble(jt54.getText());
-        sgpa[4] = Double.parseDouble(jt55.getText());
-        sgpa[5] = Double.parseDouble(jt56.getText());
-        sgpa[6] = Double.parseDouble(jt57.getText());
-        sgpa[7] = Double.parseDouble(jt58.getText());
-
-        category = jt6.getText();
-
-        Student s = new Student(name,usn,age,address,sgpa,category);
-        arr.add(s);
-        Font f = new Font("Arial",Font.PLAIN,13);
-        ja.setFont(f);
+        double cgpa = 0.0;
         String res = "";
-        for(Student itr:arr){
-            System.out.println(itr);
-            res = res + itr;
+        boolean error = false;
+        String msg = "";
+        try {
+
+            name = jt1.getText();
+
+            usn = jt2.getText();
+            age = Integer.parseInt(jt3.getText());
+            if (age <= 18) {
+                // JOptionPane.showInternalMessageDialog(frm, "Enter valid age", "Wrong Age",
+                // JOptionPane.DEFAULT_OPTION);
+                msg = msg + "Enter valid age\n";
+                // System.out.println("Enter valid age");
+            }
+            address = jt4.getText();
+            sgpa = new double[8];
+            sgpa[0] = Double.parseDouble(jt51.getText());
+            sgpa[1] = Double.parseDouble(jt52.getText());
+            sgpa[2] = Double.parseDouble(jt53.getText());
+            sgpa[3] = Double.parseDouble(jt54.getText());
+            sgpa[4] = Double.parseDouble(jt55.getText());
+            sgpa[5] = Double.parseDouble(jt56.getText());
+            sgpa[6] = Double.parseDouble(jt57.getText());
+            sgpa[7] = Double.parseDouble(jt58.getText());
+            for (int i = 0; i < 8; i++) {
+                if (sgpa[i] < 0 || sgpa[i] > 10) {
+                    error = true;
+                    // JOptionPane.showInternalMessageDialog(frm,"Enter valid SGPA","Invalid
+                    // SGPA",JOptionPane.INFORMATION_MESSAGE);
+                    msg += "Enter valid SGPA\n";
+                    // System.out.println("Enter valid SGPA");
+                    break;
+                }
+                cgpa += sgpa[i];
+            }
+            cgpa = cgpa / 8.0;
+            category = jt6.getText();
+            if (!category.equalsIgnoreCase("UG") && !category.equalsIgnoreCase("PG")) {
+                // JOptionPane.showInternalMessageDialog(frm,"Enter valid category","Invalid
+                // Category",JOptionPane.INFORMATION_MESSAGE);
+                error = true;
+                msg += "Enter valid category\n";
+                // System.out.println("Enter valid category");
+            }
+
+        } catch (NumberFormatException e) {
+            ja.setText("Wrong Details");
         }
-        ja.setText(res);
+        if (evt.getSource() == confirm) {
+            Font f = new Font("Arial", Font.PLAIN, 13);
+            ja.setFont(f);
+            for (Student itr : arr) {
+                System.out.println(itr);
+                res = res + itr;
+            }
+            ja.setText(res);
+        }
+
+        if (evt.getSource() == compute) {
+            if(!error)
+                ja.setText("CGPA:" + cgpa);
+            else    
+                JOptionPane.showInternalMessageDialog(frm.getContentPane(),"CGPA can't be calculated.\nInvalid SGPA found.","Invalid CGPA",JOptionPane.ERROR_MESSAGE);
+        }
+
+        if (evt.getSource() == done) {
+            if(!error){
+                Student s = new Student(name, usn, age, address, sgpa, category);
+                arr.add(s);
+            }else{
+                System.out.println(msg);
+                JOptionPane.showInternalMessageDialog(frm.getContentPane(), msg, "Invalid Details", JOptionPane.INFORMATION_MESSAGE);
+            }
+
+        }
 
     }
 
